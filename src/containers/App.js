74bc839fc68dae02fd207d576901f1349c19cd3e
Justin Lee <justinlee0022@gmail.com>
2019-02-06 13:08:33 -0800

    Styled according to standard

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,14 +5,12 @@ const App = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <ul className="header">
-        <li><a href="/">Home</a></li>
-        <li><a href="/studentDashboard">Student Dashboard</a></li>
-        <li><a href="/instructorDashboard">Instructor Dashboard</a></li>
+      <ul className='header'>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/studentDashboard'>Student Dashboard</a></li>
+        <li><a href='/instructorDashboard'>Instructor Dashboard</a></li>
       </ul>
-      <div className="content">
-
-      </div>
+      <div className='content' />
     </div>
   )
 }
