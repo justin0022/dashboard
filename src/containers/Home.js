d7ad6bb8e8da996f8c0cc7dc93e5ba3f7123acff
Justin Lee <justin.lee@ubc.ca>
2019-02-11 08:47:57 -0800

    Working on homepage  dashboard components

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
-const Home = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    color: theme.palette.text.secondary
+  }
+})
+
+const Home = ({ classes }) => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <Typography variant='subtitle2'>Total Logins</Typography>
+
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   )
 }
 
-export default Home
+export default withStyles(styles)(Home)
