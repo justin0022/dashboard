711024ce47f6d4abbc71a421a4ce281c03840dcd
Justin Lee <justin.lee@ubc.ca>
2019-02-11 09:42:14 -0800

    Working on dashboard line chart

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,8 @@ import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import LineChart from '../components/LineChart'
+import lineChartData from '../data/lineChartData'
 
 const styles = theme => ({
   root: {
@@ -20,12 +22,31 @@ const Home = ({ classes }) => {
       <Grid container spacing={24}>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
-            <Typography variant='subtitle2'>Total Logins</Typography>
-
+            <Grid item container>
+              <Grid item xs={12} sm={6} container direction='column' spacing={8}>
+                <Grid item>
+                  <Typography gutterBottom variant='subtitle2'>Total Logins</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography gutterBottom variant='h6'>423</Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} sm={6} container direction='column' spacing={8}>
+                <Grid item>
+                  <Typography gutterBottom variant='subtitle2' align='right'>View Report</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography gutterBottom variant='h6' align='right'>82.6%</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} container>
+              <LineChart id='lineChart1' data={lineChartData} width={300} height={200} />
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
-    </div>
+    </div >
   )
 }
 
