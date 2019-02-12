8fbbea496859f5ba2a069960768287591b3982af
Justin Lee <justin.lee@ubc.ca>
2019-02-12 10:23:21 -0800

    Added modified table component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,6 +7,7 @@ import Link from '@material-ui/core/Link'
 import LineChart from '../components/LineChart'
 import BarChart from '../components/BarChart'
 import GroupedBarChart from '../components/GroupedBarChart'
+import Table from '../components/Table'
 import lineChartData from '../data/lineChartData'
 import barChartData from '../data/barChartData'
 import groupedBarChartData from '../data/groupedBarChartData'
@@ -106,6 +107,39 @@ const Home = ({ classes }) => {
             </Grid>
             <Grid item xs={12}>
               <GroupedBarChart data={groupedBarChartData} />
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Paper className={classes.paper}>
+            <Grid item container>
+              <Grid item xs={12} sm={6} container direction='column' spacing={8}>
+                <Grid item>
+                  <Typography gutterBottom variant='subtitle2'>Total Logins</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography gutterBottom variant='h6'>423</Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} sm={6} container direction='column' spacing={8}>
+                <Grid item>
+                  <Typography gutterBottom variant='subtitle2' align='right'>
+                    <Link>View Report</Link>
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography gutterBottom variant='h6' align='right'>82.6%</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Table tableHead={['ID', 'Name', 'Salary', 'Country']}
+                tableData={[
+                  ['1', 'Dakota Rice', '$36,738', 'Niger'],
+                  ['2', 'Minerva Hooper', '$23,789', 'Curaçao'],
+                  ['3', 'Sage Rodriguez', '$56,142', 'Netherlands'],
+                  ['4', 'Philip Chaney', '$38,735', 'Korea, South']
+                ]} />
             </Grid>
           </Paper>
         </Grid>
