03282adf4339224cb6d1affab0345248c2911bc2
Justin Lee <justinlee0022@gmail.com>
2019-02-14 14:57:15 -0800

    Created 2 line chart as proof of concept

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -41,22 +41,9 @@ const Home = ({ classes }) => {
         </Grid>
         <LineChartCard dataURL={lineChartURL} feedbackId={'lineChartId'} classes={classes} >
           <Grid item container>
-            <Grid item xs={6} container direction='column' spacing={8}>
+            <Grid item xs={12} container direction='column' spacing={8}>
               <Grid item>
-                <Typography gutterBottom variant='subtitle2'>Total Logins</Typography>
-              </Grid>
-              <Grid item>
-                <Typography gutterBottom variant='h6'>423</Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={6} container direction='column' spacing={8}>
-              <Grid item>
-                <Typography gutterBottom variant='subtitle2' align='right'>
-                  <Link>View Report</Link>
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography gutterBottom variant='h6' align='right'>82.6%</Typography>
+                <Typography gutterBottom variant='h6'>Active Users</Typography>
               </Grid>
             </Grid>
           </Grid>
