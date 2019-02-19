17a7e6d13261f06c0e23322ec2603f6126a68fb8
Justin Lee <justinlee0022@gmail.com>
2019-02-19 14:34:35 -0800

    Deleted unnecessary file

--- src/components/GradesDistribution.js
+++
@@ -1,6 +0,0 @@
-import createHistogram from './d3/createHistogram'
-import withResponsiveness from '../components/hoc/withResponsiveness'
-import createChartComponent from '../components/hoc/createChartComponent'
-import compose from '../util/compose'
-
-const Histogram = createChartComponent(createHistogram)
