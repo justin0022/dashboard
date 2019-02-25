cccaee3241b732d3033164062ddc0ef19c84e907
Soo <soohyun.kim@alumni.ubc.ca>
2019-02-25 13:53:54 -0800

    wip

--- src/components/d3/createMapChart.js
+++ src/components/d3/createMapChart.js
@@ -10,13 +10,14 @@ const createMapChart = ({ data, width, height, el }) => {
     .attr('width', aWidth)
     .attr('height', aHeight)
 
-  const featureCollection = topojson.feature(data.mapChartData, data.mapChartData.objects.counties)
+  // const features = data.mapChartData.states.features
+
+  const featureCollection = topojson.feature(data, data.mapChartData)
   const heatmapStates = data.heatmapData.states
   const heatmapPopulation = {}
   for (let i = 0; i < heatmapStates.length; i++) {
     heatmapPopulation[heatmapStates[i].name] = heatmapStates[i].population
   }
-  console.log(heatmapPopulation)
 
   const projection = d3
     .geoIdentity()
@@ -31,12 +32,20 @@ const createMapChart = ({ data, width, height, el }) => {
     .enter().append('path')
     .attr('d', path)
 
-  svg.append('path')
-    .datum(topojson.mesh(data.mapChartData, data.mapChartData.objects.states, (a, b) => a !== b))
-    .attr('fill', 'none')
-    .attr('stroke', 'white')
-    .attr('stroke-linejoin', 'round')
-    .attr('d', path)
+  /*
+    d3.json(data.heatmapData, (json) => {
+      const heatmap = d3.scale.linear()
+        .domain([0, d3.max(json.features, (d) => { return Math.log(heatmapPopulation[d.properties.abbr] || 1) })])
+        .interpolate(d3.interpolateRgb)
+        .range(['#ffffff', '#073f07'])
+      const states = g.selectAll('path')
+        .data(json.features)
+        .enter().append('path')
+          .attr('d', path)
+          .attr('id', function (d) { return d.properties.abbr; })
+          .style('fill', function (d) { return heatmap(Math.log(heatmapPopulation[d.properties.abbr] || 1)) })
+    })
+  */
 }
 
 export default createMapChart
