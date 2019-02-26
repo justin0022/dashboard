4af8c24c96c621c175ef9295827bb7986f349269
Soo <soohyun.kim@alumni.ubc.ca>
2019-02-26 11:32:37 -0800

    swapped data

--- src/components/d3/createMapChart.js
+++ src/components/d3/createMapChart.js
@@ -10,9 +10,7 @@ const createMapChart = ({ data, width, height, el }) => {
     .attr('width', aWidth)
     .attr('height', aHeight)
 
-  // const features = data.mapChartData.states.features
-
-  const featureCollection = topojson.feature(data, data.mapChartData)
+  const featureCollection = data.mapChartData
   const heatmapStates = data.heatmapData.states
   const heatmapPopulation = {}
   for (let i = 0; i < heatmapStates.length; i++) {
@@ -20,7 +18,7 @@ const createMapChart = ({ data, width, height, el }) => {
   }
 
   const projection = d3
-    .geoIdentity()
+    .geoAlbersUsa()
     .fitExtent([[0, 0], [aWidth, aHeight]], featureCollection)
 
   const path = d3.geoPath()
@@ -32,20 +30,6 @@ const createMapChart = ({ data, width, height, el }) => {
     .enter().append('path')
     .attr('d', path)
 
-  /*
-    d3.json(data.heatmapData, (json) => {
-      const heatmap = d3.scale.linear()
-        .domain([0, d3.max(json.features, (d) => { return Math.log(heatmapPopulation[d.properties.abbr] || 1) })])
-        .interpolate(d3.interpolateRgb)
-        .range(['#ffffff', '#073f07'])
-      const states = g.selectAll('path')
-        .data(json.features)
-        .enter().append('path')
-          .attr('d', path)
-          .attr('id', function (d) { return d.properties.abbr; })
-          .style('fill', function (d) { return heatmap(Math.log(heatmapPopulation[d.properties.abbr] || 1)) })
-    })
-  */
 }
 
 export default createMapChart
