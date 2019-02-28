472c9e5173e151770acd84926e72ca29da3ac666
Justin Lee <justinlee0022@gmail.com>
2019-02-28 13:08:23 -0800

    Fixed bug caused by using conditional in hook at top level

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -5,15 +5,16 @@ const cache = new Map()
 
 const useData = dataURL => {
   const [data, setData] = useState(null)
-  if (cache.has(dataURL)) {
-    return cache.get(dataURL)
-  }
 
   useEffect(() => {
-    get(dataURL).then(data => {
-      cache.set(dataURL, data)
-      setData(data)
-    })
+    if (cache.has(dataURL)) {
+      setData(cache.get(dataURL))
+    } else {
+      get(dataURL).then(data => {
+        cache.set(dataURL, data)
+        setData(data)
+      })
+    }
   }, [dataURL])
 
   return data
