99c9173f29aabb2f7f339e6b5cee9a2670d9a800
Justin Lee <justinlee0022@gmail.com>
2019-02-28 09:05:23 -0800

    Added simple cache (currently getting an error in React)

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,11 +1,19 @@
 import { useState, useEffect } from 'react'
 import get from '../service/api'
 
+const cache = new Map()
+
 const useData = dataURL => {
   const [data, setData] = useState(null)
+  if (cache.has(dataURL)) {
+    return cache.get(dataURL)
+  }
 
   useEffect(() => {
-    get(dataURL).then(data => setData(data))
+    get(dataURL).then(data => {
+      cache.set(dataURL, data)
+      setData(data)
+    })
   }, [dataURL])
 
   return data
