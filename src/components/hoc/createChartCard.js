875d3354ef278476058e86987cc54d3697ccbc8c
Justin Lee <justinlee0022@gmail.com>
2019-03-18 14:01:39 -0700

    Fixed up home page

--- src/components/hoc/createChartCard.js
+++ src/components/hoc/createChartCard.js
@@ -3,12 +3,15 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Spinner from '../Spinner'
 import emojiEndpoints from '../../constants/emojiEndpoints'
+import Typography from '@material-ui/core/Typography'
 
 const createChartCard = (ChartComponent, EmojiFeedback) => props => {
   const {
     classes,
     feedbackId,
     data,
+    loaded,
+    title,
     xs = 12,
     sm = 6,
     md = 4
@@ -19,7 +22,8 @@ const createChartCard = (ChartComponent, EmojiFeedback) => props => {
       <Paper className={classes.paper}>
         {props.children}
         <Grid item xs={12}>
-          {data ? <ChartComponent data={data} {...props} /> : <Spinner />}
+          <Typography gutterBottom variant='h6'>{title}</Typography>
+          {loaded ? <ChartComponent data={data} {...props} /> : <Spinner />}
         </Grid>
         {EmojiFeedback !== undefined && data
           ? <EmojiFeedback id={feedbackId} popoverText={'give feedback'} endpoints={emojiEndpoints} />
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import useFetchAll from '../hooks/useFetchAll'
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
 import LineChart from '../components/LineChart'
-import Link from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
 import MapChart from '../components/MapChart'
 import createChartCard from '../components/hoc/createChartCard'
 import BarChart from '../components/BarChart'
 import HorizontalBarChart from '../components/HorizontalBarChart'
 import GroupedBarChart from '../components/GroupedBarChart'
-import Table from '../components/Table'
 import createToolTip from '../util/createToolTip'
 import EmojiFeedback from '../components/EmojiFeedback'
 import { barChartURL, groupedBarChartURL, lineChartURL, mapURL, sankeyURL, histogramURL, heatmapURL, scatterplotURL, donutChartURL } from '../data/gistURLs'
-import createTableCard from '../components/hoc/createTableCard'
 import Sankey from '../components/Sankey'
 import Histogram from '../components/Histogram'
 import Scatterplot from '../components/Scatterplot'
 import DonutChart from '../components/DonutChart'
+import ScatterplotWithRegressionLine from '../components/ScatterplotWithRegressionLine'
+import useFetch from '../hooks/useFetch'
 
 const styles = theme => ({
   root: {
@@ -42,106 +39,91 @@ const LineChartCard = createChartCard(LineChart, EmojiFeedback)
 const BarChartCard = createChartCard(BarChart, EmojiFeedback)
 const HorizontalBarChartCard = createChartCard(HorizontalBarChart)
 const GroupedBarChartCard = createChartCard(GroupedBarChart, EmojiFeedback)
-const TableCard = createTableCard(Table, EmojiFeedback)
 const MapChartCard = createChartCard(MapChart)
 const SankeyCard = createChartCard(Sankey)
 const HistogramCard = createChartCard(Histogram)
 const ScatterplotCard = createChartCard(Scatterplot)
 const DonutChartCard = createChartCard(DonutChart)
+const ScatterPlotWithRegressionLineCard = createChartCard(ScatterplotWithRegressionLine)
 
 function Home (props) {
   const { classes } = props
 
-  const homeData = useFetchAll(
-    lineChartURL,
-    groupedBarChartURL,
-    barChartURL,
-    mapURL,
-    heatmapURL,
-    sankeyURL,
-    histogramURL,
-    scatterplotURL,
-    donutChartURL,
-    lineChartURL
-  )
+  const [lineChartLoaded, lineChartData] = useFetch(lineChartURL)
+  const [groupedBarChartLoaded, groupedBarChartData] = useFetch(groupedBarChartURL)
+  const [barChartLoaded, barChartData] = useFetch(barChartURL)
+  const [mapLoaded, mapChartData] = useFetch(mapURL)
+  const [heatmapLoaded, heatmapData] = useFetch(heatmapURL)
+  const [sankeyLoaded, sankeyData] = useFetch(sankeyURL)
+  const [histogramLoaded, histogramData] = useFetch(histogramURL)
+  const [scatterplotLoaded, scatterplotData] = useFetch(scatterplotURL)
+  const [donutLoaded, donutData] = useFetch(donutChartURL)
 
-  const lineChartData = homeData ? homeData[0] : null
-  const groupedBarChartData = homeData ? homeData[1] : null
-  const barChartData = homeData ? homeData[2] : null
-  const mapChartData = homeData ? homeData[3] : null
-  const heatmapData = homeData ? homeData[4] : null
-  const mapData = homeData ? { mapChartData, heatmapData } : null
-  const sankeyData = homeData ? homeData[5] : null
-  const histogramData = homeData ? homeData[6] : null
-  const scatterplotData = homeData ? homeData[7] : null
-  const donutData = homeData ? homeData[8] : null
+  const mapData = { mapChartData, heatmapData }
 
   return (
     <div className={classes.root}>
       <Grid container spacing={16}>
+        <ScatterPlotWithRegressionLineCard
+          data={scatterplotData}
+          classes={classes}
+          title={'Scatterplot with Regression'}
+          loaded={scatterplotLoaded} />
         <DonutChartCard
           data={donutData}
-          classes={classes}>
-          <Typography gutterBottom variant='h6'>Donut</Typography>
-        </DonutChartCard>
+          classes={classes}
+          loaded={donutLoaded}
+          title={'Donut'} />
         <ScatterplotCard
           data={scatterplotData}
           classes={classes}
           xAxisLabel={'Grade %'}
-          yAxisLabel={'Grade %'}>
-          <Typography gutterBottom variant='h6'>Scatterplot</Typography>
-        </ScatterplotCard>
-        <LineChartCard data={lineChartData} feedbackId={'lineChartId'} classes={classes} >
-          <Typography gutterBottom variant='h6'>Active Users</Typography>
-        </LineChartCard>
-        <BarChartCard data={barChartData} feedbackId={'barChart'} classes={classes} xs={12}>
-          <Typography gutterBottom variant='h6'>Vertical Bar Chart</Typography>
-        </BarChartCard>
-        <HorizontalBarChartCard data={barChartData} classes={classes} xs={12}>
-          <Typography gutterBottom variant='h6'>Horizontal Bar Chart</Typography>
-        </HorizontalBarChartCard>
-        <GroupedBarChartCard data={groupedBarChartData} feedbackId={'groupedBarChart'} classes={classes} />
+          yAxisLabel={'Grade %'}
+          loaded={scatterplotLoaded}
+          title={'Scatterplot'} />
+        <LineChartCard
+          data={lineChartData}
+          feedbackId={'lineChartId'}
+          classes={classes}
+          loaded={lineChartLoaded}
+          title={'Line Chart'} />
+        <BarChartCard
+          data={barChartData}
+          feedbackId={'barChart'}
+          classes={classes}
+          loaded={barChartLoaded}
+          title={'Vertical Bar Chart'} />
+        <HorizontalBarChartCard
+          data={barChartData}
+          classes={classes}
+          loaded={barChartLoaded}
+          title={'Horizontal Bar Chart'} />
+        <GroupedBarChartCard
+          data={groupedBarChartData}
+          feedbackId={'groupedBarChart'}
+          classes={classes}
+          loaded={groupedBarChartLoaded}
+          title={'Grouped Bar Chart'} />
         <MapChartCard
-          data={mapData} classes={classes}
+          data={mapData}
+          classes={classes}
           tip={createToolTip(d => renderToString(
             <Paper className={classes.paper}>
               {d.properties.name}
             </Paper>
-          ))} />
-        <SankeyCard data={sankeyData} classes={classes}>
-          <Typography gutterBottom variant='h6'>Sankey</Typography>
-        </SankeyCard>
-        <HistogramCard data={histogramData} classes={classes} />
-        <TableCard tableHead={['ID', 'Name', 'Salary', 'Country']}
-          tableData={[
-            ['1', 'Dakota Rice', '$36,738', 'Niger'],
-            ['2', 'Minerva Hooper', '$23,789', 'Curaçao'],
-            ['3', 'Sage Rodriguez', '$56,142', 'Netherlands'],
-            ['4', 'Philip Chaney', '$38,735', 'Korea, South']
-          ]}
+          ))}
+          loaded={mapLoaded && heatmapLoaded}
+          title={'Map'} />
+        <SankeyCard
+          data={sankeyData}
           classes={classes}
-          feedbackId={'table'}>
-          <Grid item container>
-            <Grid item xs={6} container direction='column' spacing={8}>
-              <Grid item>
-                <Typography gutterBottom variant='subtitle2'>Table</Typography>
-              </Grid>
-              <Grid item>
-                <Typography gutterBottom variant='h6'>423</Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={6} container direction='column' spacing={8}>
-              <Grid item>
-                <Typography gutterBottom variant='subtitle2' align='right'>
-                  <Link>View Report</Link>
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography gutterBottom variant='h6' align='right'>82.6%</Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </TableCard>
+          loaded={sankeyLoaded}
+          title={'Sankey'} />
+        <HistogramCard
+          data={histogramData}
+          classes={classes}
+          loaded={histogramLoaded}
+          title={'Histogram'} />
       </Grid>
     </div >
   )
